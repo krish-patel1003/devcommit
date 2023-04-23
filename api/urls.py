6e8e5b841985344b307d55fd1e863ba0933ec156
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-23 19:21:41 +0530

    added landing_page

--- api/urls.py
+++ api/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from api.views import landing_page
 
 urlpatterns = [
+    path('', landing_page, name="landing_page"),
     path('api/admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/hackathons/', include('hackathons.urls')),
