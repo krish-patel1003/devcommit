bbfde28b962e1a114f5ce07bb6db01725f3b05c8
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-23 18:21:00 +0530

    deploy changes

--- api/settings.py
+++ api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +88,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': dj_database_url.config(
+        default='postgres://krish:1VB9vExKCUEBmyTR98xVmAlrvMtrhLdz@dpg-ch2icjjh4hsum46p5n20-a.singapore-postgres.render.com/devcommit_db',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
