146214f96b677f86c82d26cec4348dc0af3605f3
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2023-04-23 18:45:59 +0530

    static files and media files path change

--- api/settings.py
+++ api/settings.py
@@ -142,6 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, '/static/')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
@@ -172,3 +176,7 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIAFILES_DIRS = [
+   os.path.join(BASE_DIR, '/static/')
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
